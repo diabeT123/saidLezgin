91f4d349f8ab4cbf61bc1f82a1116a5b92b4fbec
diabeT123 <122596897+diabeT123@users.noreply.github.com>
2023-01-25 12:51:06 +0300

    Delete docs/CREATEX directory

--- docs/CREATEX/js/main.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    var mixer = mixitup('.directions__cards');
-
-
-    $('.directions__button-link').on('click', function(){
-        $('.directions__button-link').removeClass('directions__button-link--active')
-        $(this).addClass('directions__button-link--active')
-    })
-})
